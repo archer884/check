12e58088f897af69d7944d348e075e13fea82f84
J/A <archer884@gmail.com>
2015-08-20 15:52:58 -0500

    Comment

--- src/bench.rs
+++ src/bench.rs
@@ -6,13 +6,15 @@ lazy_static! {
 }
 
 // Based on these benchmarks, it looks to me as if `lazy_static` is definitively the faster of the
-// two options I have (realistically) for making something accessible to multiple threads.
+// two options I have (realistically) for making something accessible to multiple threads when
+// dealing with lines of the length I'm working with.
 //
 //      running 2 tests
 //      test bench::access_arc          ... bench:      36 ns/iter (+/- 41)
 //      test bench::access_lazy_static  ... bench:       0 ns/iter (+/- 1)
 //
 // So I'm gonna keep doing it that way. :p
+//
 
 #[bench]
 fn access_lazy_static(b: &mut test::Bencher) {
