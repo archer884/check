d1739d0aa8b1ba437e8f1c21cec8708e46c2065a
J/A <archer884@gmail.com>
2015-08-23 15:20:22 -0500

    Something about decolletage is broken
    Don't know what, but it can't be found via binary search and I don't know
    why that is. Need to see if this holds true on mac, so I'm going to go
    fiddle with it

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -6,7 +6,14 @@ pub trait Dictionary: Sync {
 
 impl Dictionary for Vec<String> {
     fn is_valid(&self, word: &String) -> bool {
-        self.binary_search(word).is_ok()
+        // self.binary_search(word).is_ok()
+        match self.binary_search(word) {
+            Ok(_) => true,
+            Err(idx) => {
+                println!("{} :: {}", word, self[idx]);
+                false
+            }
+        }
     }
 }
 
